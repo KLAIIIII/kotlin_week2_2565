d7ea812fe4610f2f099dc119a3b1a944a65bda11
KLAIIIII <guyyg03546@gmail.com>
2022-10-25 17:48:57 +0700

    update

---
+++ src/main/kotlin/02_Operator.kt
@@ -0,0 +1,25 @@
+fun kMessage(){
+    println("================")
+}
+
+fun kDataType() {
+    println("1.Signed Integral Type")
+    val kByte: UByte = 255u
+    val kShort:UShort = 65_533u
+    println("val kShort = $kShort")
+
+}
+
+fun kOperator(){
+    val a:Byte = 4 + 2
+    var b:Byte = 4 - 2
+    val c:Byte = 4 * 2
+    val d:Byte = 4/2
+    var e:Byte = 10%3
+    println("2.Operator in java");
+    println("val a = $a")
+    println("var b = $b")
+    println("var b = $c")
+    println("var b = $d")
+    println("var b = $e")
+}
---
+++ src/main/kotlin/05_comparativeOperator.kt
@@ -0,0 +1,8 @@
+fun comparativeOperator(){
+    val d1 = (2 == 1); println("val d1 = $d1")
+    val d2 = (2 != 1); println("val d1 = $d2")
+    val d3 = (2 > 1); println("val d1 = $d3")
+    val d4 = (2 <= 1); println("val d1 = $d4")
+    val d5 = "kotlin" == "kotlin"; println("val d1 = $d5")
+
+}
